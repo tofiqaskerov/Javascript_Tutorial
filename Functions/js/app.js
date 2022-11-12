e538c0513caa2a4db81d6852cb5b9238cb3e7382
tofiqaskerov <tofiqaskerov71@gmail.com>
2022-11-13 02:09:53 +0400

    Functions

---
+++ Functions/js/app.js
@@ -0,0 +1,34 @@
+
+
+/* 
+   function - kod bir defe teyin olunur ve coxlu istifade etmek olur.
+         kodlari isletmek ucun funksiyanin adini cagirmaq lazimdir
+*/ 
+
+/* 
+   function - Define code once, ans use it many times.
+       To perform some code, call the function name.
+
+*/
+
+startProgram()
+
+
+function startProgram(){
+    let firstName = "Tofiq"
+    let age = 19;
+    alert(firstName, age)
+}
+
+function alert(firstName, age){
+    console.log("My name is Gustavo . But you can call me Gus");
+    console.log("My name is Patrick Bateman");
+    console.log("Je suis Pablo Emilio Escobar Gaviria ");
+    console.log("I'm Rick Grimes");
+    console.log("Antonio Montana");
+    console.log("I'm Joker");
+    console.log("I'm Batman");
+    console.log("Big pun Punisher");
+    console.log("My Name is ", firstName);
+    console.log("I'm ", age , " years old");
+}
