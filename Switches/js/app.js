a5a213cb02d258af37bccc55d198d54c7b2136ad
tofiqaskerov <tofiqaskerov71@gmail.com>
2022-11-12 23:23:07 +0400

    Switches

---
+++ Switches/js/app.js
@@ -0,0 +1,35 @@
+/* 
+    switch - switch-e verilen deyer hansi serte uygundursa o bas verecek;
+    If else-den daha semerelidir;
+
+*/
+
+/* 
+    switch -statement that examines a value for a match against many case clauses.
+    More efficient that many else if statements
+
+*/
+
+
+let num1= 20 ;
+
+switch (num) {
+    case 10:
+        console.log("10");
+        break;
+    case 20:
+        console.log("20");
+        break;
+    case 30:
+        console.log("30");
+        break;
+    case 40:
+        console.log("40");
+        break;
+    case 50:
+        console.log("50");
+        break;
+    default:
+        console.log("Nothing happen");
+        break;
+}
