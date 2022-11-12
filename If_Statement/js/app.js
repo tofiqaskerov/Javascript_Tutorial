d69761d976a78a5169ef7c39c138ff1cf6fa7f8a
tofiqaskerov <tofiqaskerov71@gmail.com>
2022-11-12 04:19:12 +0400

    If_Statement

---
+++ If_Statement/js/app.js
@@ -0,0 +1,39 @@
+/* 
+   if statement - qerar qebul etmenin esas formasidir,
+       eger sert dogrudursa bir sey bas versin, yox eger dogru deyilse
+       bas vermesin
+
+*/
+
+
+/* 
+   if statement - a basic form of decision making 
+              if a condition is true, then do something if not,
+               then don't do it
+
+*/
+
+
+let count = 32;
+
+if( count > 30){
+    console.log("You accepted");
+}
+else if(count>10){
+    console.log("Please don't come back ))))")
+}
+else if(count<5){
+    console.log(":Z");
+}
+else{
+    console.log("You are not accepted!!!!");
+}
+
+
+let online = true
+if(online){
+    console.log("You are online !!!")
+}
+else{
+    console.log("You are offline !!!")
+}
