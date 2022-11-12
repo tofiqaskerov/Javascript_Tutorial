ffb7de878d4ba22bf0d5a76cd0d599b67bdbb972
tofiqaskerov <tofiqaskerov71@gmail.com>
2022-11-12 04:07:48 +0400

    Method_Chaining

---
+++ Method_Chaining/js/app.js
@@ -0,0 +1,13 @@
+
+
+// method chaining - ardicil kod setrinde bir methodun digerinin ardinca cagirilmasina deyilir ---- calling one method after another in one continuous line of code
+
+let firstName = "Tofiq"
+
+let letter = firstName.charAt(0).toLocaleLowerCase().trim()
+console.log(letter);
+
+
+// let letter = firstName.charAt(0);
+// letter = letter.toLocaleLowerCase()
+// console.log(letter);
