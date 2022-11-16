16723836564c7c3716085fe00b4c7f9e2886dbd5
tofiqaskerov <tofiqaskerov71@gmail.com>
2022-11-17 00:51:41 +0400

    Array_Reduce

---
+++ Array_Reduce/js/app.js
@@ -0,0 +1,21 @@
+/* 
+   array.reduce() = method funksiyanin tek deyerini qaytarir
+*/
+
+
+/* 
+  array.reduce() =  reduces an array to a single value
+*/
+
+let prices = [5,50,24,64,55]
+
+let total = prices.reduce((total, e) =>{
+    console.log(total);
+    return total += e
+ },0)
+console.log(total);
+
+
+
+
+
