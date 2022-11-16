34a85f2446a9cf0a31cfede4a538589fb8cbf62c
tofiqaskerov <tofiqaskerov71@gmail.com>
2022-11-16 23:06:21 +0400

    Array_Map

---
+++ Array_Map/js/app.js
@@ -0,0 +1,34 @@
+/* 
+   array.map() =  metodu arrayin hər bir elementi
+    üçün funksiya çağırır ve yeni bir array yaradir.
+    executes a provided callback function once for each array element and creates a new array
+*/
+
+
+/* 
+
+  array.map() =  executes a provided callback function once for
+   each array element and creates a new array
+
+*/
+
+
+let nums = [1,4,8,9,2];
+let squares = nums.map(square)
+let cubes = nums.map(cube)
+
+function cube(e){
+    return e*e*e
+}
+function square(e){ 
+    return e*e
+}
+squares.forEach((e) =>{
+    console.log(e);
+})
+console.log("-------------------");
+cubes.forEach((e) =>{
+    console.log(e);
+})
+
+ 
