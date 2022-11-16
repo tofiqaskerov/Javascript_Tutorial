4d6b48a89403cb9b7dbac3f859cea4fbc30491d7
tofiqaskerov <tofiqaskerov71@gmail.com>
2022-11-16 23:28:29 +0400

    Array_Filter

---
+++ Array_Filter/js/app.js
@@ -0,0 +1,22 @@
+
+
+/* 
+   array.filter() = funksiyanın teyin etdiyi sertden 
+   keçən bütün elementlər olan yeni array yaradır
+*/
+
+/* 
+   array.filter() = creates a new array with
+    all elements that pass the test provided by a function
+*/
+
+
+
+let ages = [20,50,30,41,15]
+let adults = ages.filter(checkAge);
+adults.forEach(e => {
+   console.log(e);
+});
+function checkAge(e){
+   return e > 30
+}
